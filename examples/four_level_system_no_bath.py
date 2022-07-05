b27eb2d435927ab662898579881a28c289028fd7
LachlanD-K <lachlandufort-kennett@outlook.com>
2022-07-05 14:31:37 +0100

    Added some corrections

--- examples/four_level_system_no_bath.py
+++ examples/four_level_system_no_bath.py
@@ -14,7 +14,7 @@ def four_level_system_dynamics(end_time=10.0):
     # define constants and system parameters
     # \hbar == k_b == 1
     end_time = end_time
-    alpha = 0.25  # coupling strength
+    alpha = 0.0  # coupling strength
     temperature = 0.026  # environment temperature
     nu_c = 0.15  # environment cutoff frequency
     omega_0 = 0.0  # two-level system frequency (set to 0)
@@ -22,11 +22,11 @@ def four_level_system_dynamics(end_time=10.0):
     sqrt_s = 1.0  # Huang-Rhys parameter (strength of light-matter coupling)
     omega_c = 0.0  # cavity frequency
     kappa = 0.01  # field decay
-    # Gamma_up = 0.01  # incoherent gain (electronic gain)
-    # Gamma_down = 0.01  # incoherent loss (electronic dissipation)
+    Gamma_up = 0.01  # incoherent gain (electronic gain)
+    Gamma_down = 0.01  # incoherent loss (electronic dissipation)
     omega_v = 1.0  # vibrational system frequency
-    v_gamma_up = 0.0244+0.0555j  # from calc_gamma.py
-    v_gamma_down = 1.63e-06-0.0193j
+    v_gamma_up = 1.63e-06  # from calc_gamma.py
+    v_gamma_down = 0.0244
 
     # compute operators
     I_2 = oqupy.operators.identity(2)
@@ -38,10 +38,10 @@ def four_level_system_dynamics(end_time=10.0):
     tau_plus = np.kron(I_2, oqupy.operators.sigma("+"))
     tau_minus = np.kron(I_2, oqupy.operators.sigma("-"))
     tau_up_dm = np.matmul(tau_plus, tau_minus)
-    tau_x = (tau_plus + tau_minus) / np.sqrt(2)
+    tau_x = tau_plus + tau_minus
 
     # compute dynamics
-    initial_field = 1.0 + 1.0j
+    initial_field = np.sqrt(0.05)
     initial_state = np.matmul(np.matmul(sigma_minus, sigma_plus),
                               np.matmul(tau_minus, tau_plus))  # electronic and vibrational unexcited
 
@@ -54,8 +54,9 @@ def four_level_system_dynamics(end_time=10.0):
         expectation_value_sigma_minus = np.matmul(sigma_minus, state).trace()
         return -(1j * omega_c + kappa) * a - 0.5j * gn * expectation_value_sigma_minus
 
-    gammas = [lambda t: v_gamma_down, lambda t: v_gamma_up]
-    lindblad_operators = [lambda t: np.sqrt(v_gamma_down) * (tau_minus + sqrt_s * np.matmul(sigma_plus, sigma_minus)),
+    gammas = [lambda t: Gamma_down, lambda t: Gamma_up, lambda t: v_gamma_down, lambda t: v_gamma_up]
+    lindblad_operators = [lambda t: sigma_minus, lambda t: sigma_plus,
+                          lambda t: np.sqrt(v_gamma_down) * (tau_minus + sqrt_s * np.matmul(sigma_plus, sigma_minus)),
                           lambda t: np.sqrt(v_gamma_up) * (tau_plus + sqrt_s * np.matmul(sigma_plus, sigma_minus))]
 
     td_system = oqupy.TimeDependentSystemWithField(Hamiltonian, field_EOM, gammas=gammas,
