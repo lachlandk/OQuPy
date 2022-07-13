805d6083722aa1a91359184ab0e144ebe1e8ad99
LachlanD-K <lachlandufort-kennett@outlook.com>
2022-07-13 13:46:03 +0100

    Added figure title with parameters

--- examples/four_level_system_SR_coupling.py
+++ examples/four_level_system_SR_coupling.py
@@ -10,8 +10,7 @@ sys.path.insert(0, "../")
 
 import oqupy
 
-
-def four_level_system_dynamics(end_time=10.0):
+if __name__ == "__main__":
     # define constants and system parameters
     # \hbar == k_b == 1
     parameters_file = open("parameters.json")
@@ -33,7 +32,7 @@ def four_level_system_dynamics(end_time=10.0):
     dkmax = parameters["dkmax"]
     epsrel = 10 ** parameters["epsrel"]
     parameters_file.close()
-    
+
     # compute operators
     I_2 = oqupy.operators.identity(2)
 
@@ -48,16 +47,20 @@ def four_level_system_dynamics(end_time=10.0):
 
     # compute dynamics
     initial_field = np.sqrt(0.05)
-    initial_state = np.matmul(np.matmul(sigma_minus, sigma_plus), np.matmul(tau_minus, tau_plus))  # electronic and vibrational unexcited
+    initial_state = np.matmul(np.matmul(sigma_minus, sigma_plus),
+                              np.matmul(tau_minus, tau_plus))  # electronic and vibrational unexcited
+
 
     def Hamiltonian(_, a):
         return omega_0 * sigma_up_dm + gn * (
                 a * sigma_plus + np.conj(a) * sigma_minus) + omega_v * (
                        tau_up_dm + sqrt_s * np.matmul((sigma_plus + sigma_minus), tau_x))
 
+
     def field_EOM(_, state, a):
         expectation_value_sigma_minus = np.matmul(sigma_minus, state).trace()
         return -(1j * omega_c + kappa) * a - 0.5j * gn * expectation_value_sigma_minus
+
 
     gammas = [lambda t: Gamma_down, lambda t: Gamma_up]
     lindblad_operators = [lambda t: sigma_minus, lambda t: sigma_plus]
@@ -66,7 +69,7 @@ def four_level_system_dynamics(end_time=10.0):
                                                    lindblad_operators=lindblad_operators)
     correlations = oqupy.PowerLawSD(alpha=alpha, zeta=1, cutoff=nu_c, cutoff_type="gaussian",
                                     temperature=temperature)
-    bath = oqupy.Bath(1/np.sqrt(2) * tau_x, correlations)
+    bath = oqupy.Bath(1 / np.sqrt(2) * tau_x, correlations)
     tempo_parameters = oqupy.TempoParameters(dt=dt, dkmax=dkmax, epsrel=epsrel)
     # process_tensor = oqupy.pt_tempo_compute(bath=bath, start_time=0.0, end_time=end_time, parameters=pt_tempo_parameters)
 
@@ -84,38 +87,23 @@ def four_level_system_dynamics(end_time=10.0):
                                     initial_state=initial_state,
                                     start_time=0.0,
                                     parameters=tempo_parameters)
-    dynamics = tempo_sm.compute(end_time=tf)
+    dynamics = tempo_sm.compute(end_time=1500)
 
-    return {
-        "times": dynamics.times,
-        "expectations": {
-            "sigma_z": dynamics.expectations(np.kron(oqupy.operators.sigma("z"), I_2), real=True)[1],
-            "tau_dagger_tau": dynamics.expectations(np.matmul(tau_plus, tau_minus), real=True)[1],
-            "tau+tau_dagger": dynamics.expectations(tau_minus + tau_plus, real=True)[1],
-            "field": dynamics.field_expectations()[1]
-        }
-    }
-
-
-if __name__ == "__main__":
-    system = four_level_system_dynamics(end_time=1500)
-    times = system["times"]
-    sigma_z = system["expectations"]["sigma_z"]
-    tau_dagger_tau = system["expectations"]["tau_dagger_tau"]
-    tau_p_tau_dagger = system["expectations"]["tau+tau_dagger"]
-    field = system["expectations"]["field"]
+    times = dynamics.times
+    trace_delta = np.abs(np.trace(dynamics.states[-1]) - np.trace(dynamics.states[0]))
 
     fig, axes = plt.subplots(4, figsize=(10, 14))
-    axes[0].plot(times, sigma_z)
-    axes[1].plot(times, tau_dagger_tau)
-    axes[2].plot(times, tau_p_tau_dagger)
-    axes[3].plot(times, np.abs(field) ** 2)
+    fig.suptitle(f"Four level system SR coupling: sqrt_s={sqrt_s}, $\\nu_c$={nu_c}, $\\omega_v$={omega_v}, $\\omega_c$={omega_c}, trace delta = {trace_delta:.3f}")
+    axes[0].plot(times, dynamics.expectations(np.kron(oqupy.operators.sigma("z"), I_2), real=True)[1])
+    axes[1].plot(times, dynamics.expectations(np.matmul(tau_plus, tau_minus), real=True)[1])
+    axes[2].plot(times, dynamics.expectations(tau_minus + tau_plus, real=True)[1])
+    axes[3].plot(times, np.abs(dynamics.field_expectations()[1]) ** 2)
     axes[0].set_ylabel("$\\langle S_z\\rangle$")
-    axes[1].set_ylabel("$\\tau^\\dagger \\tau$")
-    axes[2].set_ylabel("$\\tau+\\tau^\\dagger$")
+    axes[1].set_ylabel("$\\langle\\tau^\\dagger \\tau\\rangle$")
+    axes[2].set_ylabel("$\\langle\\tau+\\tau^\\dagger\\rangle$")
     axes[3].set_ylabel("$n$")
     axes[3].set_xlabel("$t$")
     plt.tight_layout()
 
-    fig.savefig(os.path.join("figures", "4_level_system_SR_coupling.png"), bbox_inches="tight")
+    fig.savefig(os.path.join("figures", f"4_level_system_SR_coupling_sqrt_s{sqrt_s}_nu_c{nu_c}_omega_v{omega_v}_omega_c{omega_c}.png"), bbox_inches="tight")
     plt.show()
